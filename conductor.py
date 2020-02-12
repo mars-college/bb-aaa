c36edc820249987bf3894f647815b13ad977c51a
genekogan <kogan.gene@gmail.com>
2020-02-11 21:05:44 -0800

    new conductor/worker setup

---
+++ conductor.py
@@ -0,0 +1,180 @@
+from threading import Thread
+import argparse
+import os
+import json
+import time
+import asyncio
+import websockets
+import functools
+import queue
+from PIL import Image
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import syft as sy
+from syft.workers.websocket_client import WebsocketClientWorker
+
+class CheckableQueue(queue.Queue): 
+    def __contains__(self, item):
+        with self.mutex:
+            return item in self.queue
+
+
+
+
+
+class VAE(nn.Module):
+    
+    def __init__(self, image_size=784, h_dim=400, z_dim=20):
+        super(VAE, self).__init__()
+        self.fc1 = nn.Linear(image_size, h_dim)
+        self.fc2 = nn.Linear(h_dim, z_dim)
+        self.fc3 = nn.Linear(h_dim, z_dim)
+        self.fc4 = nn.Linear(z_dim, h_dim)
+        self.fc5 = nn.Linear(h_dim, image_size)
+
+    def encode(self, x):
+        h = F.relu(self.fc1(x))
+        return self.fc2(h), self.fc3(h)
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(log_var/2)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+        
+    def decode(self, z):
+        h = F.relu(self.fc4(z))
+        return F.sigmoid(self.fc5(h))
+
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        x_reconst = self.decode(z)
+        return x_reconst, mu, log_var
+
+    
+
+
+
+
+    
+async def handle_worker(websocket, path, hook, workers, update_queue, model, optimizer):
+    message = await websocket.recv()
+    message = json.loads(message)
+
+    action, name = message['action'], message['name']
+
+    if action == 'register':
+        host, syft_port = message['host'], message['syft_port']
+        print('register for', name, host, syft_port)
+        new_worker = WebsocketClientWorker(host=host, hook=hook, id=name, port=syft_port, log_msgs=True, verbose=True, is_client_worker=False)
+        workers[name] = new_worker
+        optimizer[workers[name]] = torch.optim.Adam(model.parameters(), lr=1e-3)
+    
+        message = json.dumps({'success': True})
+        await websocket.send(message)
+
+    elif action == 'request_model':
+        update_queue.put(name)
+        message = json.dumps({'success': True})
+        await websocket.send(message)
+
+    elif action == 'check_if_in_queue':
+        message = json.dumps({'in_queue': name not in update_queue})
+        await websocket.send(message)
+
+
+async def update_model(loop: asyncio.AbstractEventLoop, workers, model, optimizer, update_queue) -> None:
+    while True:       
+        print("queue is", update_queue.empty(), update_queue.qsize())
+        while not update_queue.empty():
+            next_worker_name = update_queue.queue[0]
+            worker = workers[next_worker_name]
+
+            print('UPDATE for %s' % next_worker_name, worker)
+
+
+#            time.sleep(20)
+
+            print('model 1')
+            model.send(worker)
+            print('model 2')
+            x, y = worker.search('#x')[0], worker.search('#y')[0]
+            print('model 3')
+
+            #x, y = x.to(device).view(-1, image_size), y.to(device)
+
+            #model.send(px.location) 
+            print('-----')
+
+            print('model 4')
+            x_reconst, mu, log_var = model(x)
+            print('model 5')
+
+            reconst_loss = F.binary_cross_entropy(x_reconst, x, size_average=False)
+            print('model 6')
+            kl_div = - 0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+            print('model 7')
+            
+            # backward
+            print('model 8')
+            loss = reconst_loss + kl_div
+            print('model 9')
+            optimizer[worker].zero_grad()
+            print('model 10')
+            loss.backward()
+            print('model 11')
+            optimizer[worker].step()
+            print('model 12')
+
+            model.get()
+
+
+            print("Reconst Loss: {:.4f}, KL Div: {:.4f}".format(reconst_loss.get(), kl_div.get()))
+
+
+
+            update_queue.get()
+            print("--->finished update for, now len", update_queue.qsize())
+        time.sleep(1)
+
+
+
+def start_background_loop(loop: asyncio.AbstractEventLoop, workers, model, optimizer, update_queue) -> None:
+    asyncio.set_event_loop(loop)
+    asyncio.run_coroutine_threadsafe(update_model(loop, workers, model, optimizer, update_queue), loop)
+    loop.run_forever()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run websocket client.")
+    parser.add_argument("--server_port", "-p", type=int, default=8765, help="port number to send messages to conductor, e.g. --msg_port 8765")    
+    parser.add_argument("--image_size", "-i", type=int, default=784, help="image size")    
+    parser.add_argument("--h_dim", "-d", type=int, default=300, help="neurons in hidden layers")    
+    parser.add_argument("--z_dim", "-z", type=int, default=20, help="dimensionality of latent space")    
+    parser.add_argument("--learning_rate", "-l", type=float, default=1e-3, help="learning rate of optimizer")    
+    args = parser.parse_args()
+    
+    # initialize model and workers
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = VAE(image_size=args.image_size, h_dim=args.h_dim, z_dim=args.z_dim).to(device)
+    optimizer = {}
+    workers, update_queue = {}, CheckableQueue()
+    
+    # setup update thread
+    loop = asyncio.new_event_loop()
+    update_thread = Thread(target=start_background_loop, args=(loop, workers, model, optimizer, update_queue), daemon=True)
+    update_thread.start()
+
+    # start update thread
+    hook = sy.TorchHook(torch)
+    start_server = websockets.serve(
+        functools.partial(handle_worker, hook=hook, workers=workers, update_queue=update_queue, model=model, optimizer=optimizer),
+        'localhost', args.server_port)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
+if __name__== "__main__":
+    main()
---
+++ worker.py
@@ -0,0 +1,170 @@
+from threading import Thread
+from socket import gethostbyname_ex, gethostname
+from enum import Enum
+import argparse
+import time
+import asyncio
+import websockets
+import os
+import json
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import syft as sy
+from syft.workers.websocket_server import WebsocketServerWorker
+
+
+
+
+class Worker:
+
+    class Mode(Enum):
+        CAPTURING = 1
+        READY_TO_UPDATE = 2
+        WAITING = 3
+
+    def __init__(self, name, conductor_ip, conductor_port, syft_port, batch_size, hook):
+        self.name = name
+        self.conductor_ip = conductor_ip
+        self.conductor_port = conductor_port
+        self.syft_port = syft_port
+        self.batch_size = batch_size
+        self.hook = hook
+
+        self.registered = False
+        self.active = False
+
+        self.captures = []
+        self.n_updates = 0
+        self.socket_uri = 'ws://%s:%d' % (conductor_ip, conductor_port)
+
+    def set_mode(self, mode):
+        self.mode = mode
+            
+    def setup_data(self, image_size):
+        self.image_size = image_size
+        trans = transforms.ToTensor() #transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+        mnist_loader = torch.utils.data.DataLoader(
+            dataset=datasets.MNIST(root='./data', train=True, transform=trans, download=True), 
+            batch_size=self.batch_size, shuffle=True)
+        self.mnist_iterator = iter(mnist_loader)
+
+    def ready_to_update(self):
+        ready = (self.num_captures() == self.batch_size)
+        return ready
+
+    def get_next_batch(self):
+        print('Get next batch of mnist')
+        x, y = next(self.mnist_iterator)
+        x = x.view(-1, self.image_size)
+        self.x_ptr = x.tag('#x').send(self.local_worker)
+        self.y_ptr = y.tag('#y').send(self.local_worker)
+        
+    def activate(self):
+        kwargs = { "hook": self.hook,
+            "id": self.name,
+            "host": self.conductor_ip,
+            "port": self.syft_port }
+        self.active = True
+        self.local_worker = WebsocketServerWorker(**kwargs)
+        self.local_worker.start()
+
+    async def capture(self):
+        print('run the camera')
+        image = None
+        self.captures.append(image)
+
+    def num_captures(self):
+        return len(self.captures)
+
+    async def try_register(self):
+        print('Register with conductor')
+        async with websockets.connect(self.socket_uri) as websocket:
+            message = json.dumps({'action': 'register', 'name': self.name, 'syft_port': self.syft_port, 'host': gethostbyname_ex(gethostname())[-1][-1]})
+            await websocket.send(message)
+            result = json.loads(await websocket.recv())
+            if result['success']:
+                print('Registration successful.')
+                self.registered = True
+
+    async def request_update(self):
+        print('Request to make a model update')
+        async with websockets.connect(self.socket_uri) as websocket:
+            message = json.dumps({'action': 'request_model', 'name': self.name})
+            await websocket.send(message)
+            result = json.loads(await websocket.recv())
+            if result['success']:
+                print('ready to make an update...')
+    
+    async def check_if_in_queue(self):
+        async with websockets.connect(self.socket_uri) as websocket:
+            message = json.dumps({'action': 'check_if_in_queue', 'name': self.name})
+            await websocket.send(message)
+            result = json.loads(await websocket.recv())
+            return result['in_queue']
+
+
+
+async def update_worker(loop: asyncio.AbstractEventLoop, worker) -> None:
+
+    while not worker.registered:
+
+        await worker.try_register()
+        time.sleep(1)
+
+    while worker.active:
+
+        if worker.mode == Worker.Mode.CAPTURING:
+            await worker.capture()
+            if worker.ready_to_update():
+                worker.set_mode(Worker.Mode.READY_TO_UPDATE)
+
+        elif worker.mode == Worker.Mode.READY_TO_UPDATE:
+            worker.get_next_batch()
+            await worker.request_update()
+            worker.set_mode(Worker.Mode.WAITING)
+
+        elif worker.mode == Worker.Mode.WAITING:
+            done = await worker.check_if_in_queue()
+            if done:
+                worker.n_updates += 1
+                worker.captures = []
+                worker.set_mode(Worker.Mode.CAPTURING)
+
+        time.sleep(1)
+    
+
+def start_background_loop(loop: asyncio.AbstractEventLoop, worker) -> None:
+    asyncio.set_event_loop(loop)
+    asyncio.run_coroutine_threadsafe(update_worker(loop, worker), loop)
+    loop.run_forever()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run websocket server worker.")
+    parser.add_argument("--name", type=str, help="name of the worker, e.g. worker001")
+    parser.add_argument("--conductor_ip", type=str, default="0.0.0.0", help="host for the socket connection")
+    parser.add_argument("--conductor_port", type=int, default=8765, help="port number of the websocket server worker, e.g. --conductor_port 8765")
+    parser.add_argument("--syft_port", type=int, help="port for syft worker")
+    parser.add_argument("--batch_size", type=int, default=8, help="number of batches")
+    parser.add_argument("--verbose", "-v", action="store_true", help="if set, websocket server worker will be started in verbose mode")
+    
+    args = parser.parse_args()
+    
+    # setup worker
+    hook = sy.TorchHook(torch)
+    worker = Worker(args.name, args.conductor_ip, args.conductor_port, args.syft_port, args.batch_size, hook)    
+    worker.setup_data(784)
+    worker.set_mode(Worker.Mode.CAPTURING)
+
+    # setup update thread
+    loop = asyncio.new_event_loop()
+    update_thread = Thread(target=start_background_loop, args=(loop, worker,), daemon=True)
+    update_thread.start()
+
+    # launch syft worker
+    worker.activate()
+
+    
+if __name__== "__main__":
+    main()
